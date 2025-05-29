8e130b2b7fa0fee19d4b3cff65a5f5878f002f8e
dsabramov <yadmabramov@yandex.ru>
2025-05-30 01:45:14 +0300

    Iter6

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,21 +4,28 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/yadmabramov/admAlerting/internal/handlers"
+	"github.com/yadmabramov/admAlerting/internal/server/log_middleware"
 	"github.com/yadmabramov/admAlerting/internal/service"
 	"github.com/yadmabramov/admAlerting/internal/storage"
+	"go.uber.org/zap"
 )
 
 func NewServer(addr string) *http.Server {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
 	storage := storage.NewMemoryStorage()
 	service := service.NewMetricsService(storage)
 	handler := handlers.NewMetricsHandler(service)
 
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 
-	// Роуты
+	r.Use(log_middleware.LoggerMiddleware(logger)) // используем напрямую
+
 	r.Get("/", handler.HandleIndex)
 	r.Post("/update/{type}/{name}/{value}", handler.HandleUpdate)
 	r.Get("/value/{type}/{name}", handler.HandleGetMetric)
